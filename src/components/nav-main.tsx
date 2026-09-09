3342f51d055f1325024b3d95c04422240ac437d3
koushik bharati <koushikbharati7@gmail.com>
2026-09-09 11:13:53 +0530

    update: sidebar active state ui

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -28,7 +28,7 @@ export function NavMain({ items }: { items: NavItem[] }) {
           return (
             <SidebarMenuItem key={item.title}>
               <SidebarMenuButton
-                className="data-active:text-primary hover:data-active:text-primary"
+                className="data-active:bg-primary/5 data-active:text-primary hover:data-active:bg-primary/5 hover:data-active:text-primary"
                 tooltip={item.title}
                 isActive={isActive}
                 render={
